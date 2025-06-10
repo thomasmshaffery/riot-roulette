9586849ee45b9e7eec3117c50f34962ca67daf70
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-06-10 18:46:59 -0400

    Button component added, swapped background15

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,25 @@
+import Button from "../components/Button";
 import PageNav from "../components/PageNav";
 import styles from "./Homepage.module.css";
 
 function Homepage() {
+  const background =
+    'linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0)), url("background' +
+    Math.floor(Math.random() * 30 + 1) +
+    '.jpg")';
+
   return (
-    <div className={styles.homepage}>
+    <div style={{ backgroundImage: background }} className={styles.homepage}>
       <PageNav />
-      Riot Roulette
+      <section>
+        <h1>RIOT ROULETTE</h1>
+        <h2>
+          Discover new looks and enjoy your classics in this fun Riot themed
+          roulette! Find new skins from League of Legends or artwork for your
+          favorite Valorant operators.
+        </h2>
+        <Button />
+      </section>
     </div>
   );
 }
