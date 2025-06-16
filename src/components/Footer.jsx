c6c3447a5fa742cba7be1e08f77e61a5d2d0adb7
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-06-16 07:34:18 -0400

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,12 @@
+import styles from "./Footer.module.css";
+
 function Footer() {
-  return <footer>Riot Roullette developed by a troll.</footer>;
+  return (
+    <footer className={styles.footer}>
+      Riot Roulette was created under Riot Games' "Legal Jibber Jabber" policy
+      using assets owned by Riot Games. Riot Games does not endorse or sponsor
+      this project.
+    </footer>
+  );
 }
 export default Footer;
