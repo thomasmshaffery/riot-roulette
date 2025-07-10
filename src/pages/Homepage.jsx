c88ffec5f12e5b1a78b5b8f582d2735513ea0f0b
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-07-10 15:57:33 -0400

    homepage mobile formatted

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,7 +2,6 @@ import { Link } from "react-router-dom";
 import Button from "../components/Button";
 import Footer from "../components/Footer";
 import PageNav from "../components/PageNav";
-import styles from "./Homepage.module.css";
 
 function Homepage({ isLoaded }) {
   const background =
@@ -18,16 +17,19 @@ function Homepage({ isLoaded }) {
       {isLoaded}
       <div className="flex flex-col min-h-svh justify-between">
         <PageNav />
-        <section className="h-8 gap-3.5 place-items-center">
-          <h1 className="text-white pb-80">RIOT ROULETTE</h1>
-          <h2 className="text-white max-w-3/5 font-sans text-sm">
+        <section className="h-8 relative bottom-5 place-items-center">
+          <h1 className="text-white text-4xl relative bottom-40 pb-80 sm:text-8xl">
+            RIOT ROULETTE
+          </h1>
+          <h2 className="text-white text-center max-w-3/4 relative bottom-20 font-sans text-sm/6 sm:max-w-2/4">
             This roulette application will display a random champion skin for
             League of Legends and a random agent for Valorant. The homepage
-            cycles between artwork from both games randomly. Thank you for
-            visiting and I hope you enjoy it!
+            cycles between artwork from both games randomly. This project was
+            developed using React, Tailwind, Valorant-API and Riot Games Data
+            Dragon as well as assets provided by Riot Games.
           </h2>
           <Link to="League">
-            <Button>DISCOVER</Button>
+            <Button className="">DISCOVER</Button>
           </Link>
         </section>
         <Footer className="h-5" />
