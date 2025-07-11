bade3533720056cfb0c415cbfb2abbce0164922e
tomshaffery <98631104+thomasmshaffery@users.noreply.github.com>
2025-07-11 02:35:11 -0400

    valorant mobile optimized

--- src/pages/Valorant.jsx
+++ src/pages/Valorant.jsx
@@ -2,7 +2,6 @@ import Footer from "../components/Footer";
 import PageNav from "../components/PageNav";
 import styles from "./Valorant.module.css";
 import Agent from "../components/Agent";
-import Button from "../components/Button";
 import { useEffect, useState } from "react";
 
 function Valorant({ agents }) {
@@ -11,6 +10,9 @@ function Valorant({ agents }) {
   const [isLoaded, setIsLoaded] = useState(false);
   const filtered = agents.data.filter((agent) => agent.isPlayableCharacter);
   const MAX = filtered.length;
+
+  const background = `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0)),
+    url("/valorant_background.jpg")`;
 
   function generateRandomNumber() {
     return Math.floor(Math.random() * MAX);
@@ -32,25 +34,49 @@ function Valorant({ agents }) {
   );
 
   return (
-    <div className={styles.valorant}>
-      <PageNav />
-      <section>
-        {!currentAgent && <h1>Roll an Agent!</h1>}
-        {currentAgent && currentAgent.isPlayableCharacter ? (
-          <Agent agent={currentAgent} />
-        ) : (
-          ""
-        )}
-        {isLoaded && (
-          <button
-            className={styles.button}
-            onClick={() => handleSetRandomNumber()}
-          >
-            Roll Agent
-          </button>
-        )}
-      </section>
-      <Footer />
+    <div
+      style={{
+        backgroundImage: background,
+      }}
+      className="bg-cover bg-center w-full h-full"
+    >
+      <div className="flex flex-col relative min-h-svh justify-between">
+        <PageNav />
+        <section className="h-8 place-items-center">
+          {isLoaded}
+          {!currentAgent && (
+            <div className="place-items-center">
+              {" "}
+              <h1 className="text-white text-center font-[Valorant] text-4xl relative bottom-30 pb-80 sm:text-8xl">
+                Roll an Agent!
+              </h1>
+              <button
+                className={styles.button}
+                onClick={() => handleSetRandomNumber()}
+              >
+                Roll Agent
+              </button>
+            </div>
+          )}
+          {currentAgent && currentAgent.isPlayableCharacter ? (
+            <div className="place-items-center relative bottom-65 text-white text-center">
+              <Agent
+                agent={currentAgent}
+                className="h-60dvh w-80dvw sm:h-40dvh w-40dvh"
+              />
+              <button
+                className={styles.button}
+                onClick={() => handleSetRandomNumber()}
+              >
+                Roll Agent
+              </button>
+            </div>
+          ) : (
+            ""
+          )}
+        </section>
+        <Footer />
+      </div>
     </div>
   );
 }
